b446a4bfe9f17eef7c7b0b215a2cbeeb2571c95b
Doraswamy raju <doraswamyraju.ca@gmail.com>
2026-08-15 17:31:26 +0530

    fix(android): remove conflicting foojay resolver causing build exception

--- sbr android/settings.gradle.kts
+++ sbr android/settings.gradle.kts
@@ -8,9 +8,6 @@ pluginManagement {
     }
     // CORRECTED: The conflicting 'plugins' block has been removed from here.
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
